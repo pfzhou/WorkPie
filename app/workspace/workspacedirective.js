1dd5fc395f57d4d830ccb7cfff521aca44c25879
pfzhou <pfzhou@gmail.com>
2015-05-19 23:35:40 +0800

    form ready

--- app/workspace/workspacedirective.js
+++ app/workspace/workspacedirective.js
@@ -5,7 +5,6 @@ var workspaceDirective = angular.module('workspaceDirective', []);
 workspaceDirective.directive('workspace', function () {
     return {
         restrict: 'E',
-        //template: '<div ng-transclude>main</div>',
         templateUrl: 'workspace/workspace.html',
         //replace: true,
         transclude: true
@@ -15,29 +14,20 @@ workspaceDirective.directive('workspace', function () {
 workspaceDirective.directive('workspaceleft', function () {
     return {
         restrict: 'E',
-        //replace: true,
-        //template: '<div>Hi there <span ng-transclude></span></div>',
-        templateUrl: 'workspace/workspaceleft.html',
-        transclude: true
+        templateUrl: 'workspace/workspaceleft.html'
     };
 });
 //主导航列表
 workspaceDirective.directive('workspacelist', function () {
     return {
         restrict: 'E',
-        //replace: true,
-        //template: '<div>Hi there <span ng-transclude></span></div>',
-        templateUrl: 'workspace/workspacelist.html',
-        transclude: true
+        templateUrl: 'workspace/workspacelist.html'
     };
 });
 //文档窗口
 workspaceDirective.directive('workspacedoc', function () {
     return {
         restrict: 'E',
-        //replace: true,
-        //template: '<div>Hi there <span ng-transclude></span></div>',
-        templateUrl: 'workspace/workspacedoc.html',
-        transclude: true
+        templateUrl: 'workspace/workspacedoc.html'
     };
 });
